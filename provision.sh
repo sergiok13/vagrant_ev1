83e14bd3d8ab79c531ab982da974e5e04f4e633b
sergiok13 <sergiotorrome09@gmail.com>
2020-10-28 16:57:26 +0100

    copio el fichero default de sites-enabled default dentro de la carpeta /vagrant/sites-enabled  y en el provision borro el fichero anterior a las modificaciones, que vuelvo a copiar desde el provision para que se quede guardada la nueva configuración, que tiene una ruta nueva para cargar el index.html y gracias a eso quito el comando que se encarga de crear el enlace blando, ya que pilla directamente el de /vagrant/www

--- provision.sh
+++ provision.sh
@@ -2,15 +2,15 @@ apt-get -y update
 
 apt-get -y install nginx
 
+mkdir /vagrant/sites-enabled
+
+rm -rf /etc/nginx/sites-enabled
+
+cp -r /vagrant/sites-enabled /etc/nginx
+
 cp -r /usr/share/nginx/www /vagrant
 
 rm -r /usr/share/nginx/www
 
-ln -s /vagrant/www /usr/share/nginx/www
-
 service nginx start
-
-mkdir /vagrant/sites-enabled
-
-cp /etc/nginx/sites-enabled/default /vagrant/sites-enabled
 
