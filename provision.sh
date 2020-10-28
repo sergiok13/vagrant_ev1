b8853191e5d2bc564fcef2c62f7bd9a979ff448c
sergiok13 <sergiotorrome09@gmail.com>
2020-10-28 15:52:58 +0100

    Copio el fichero default de la ruta /etc/nginx/sites-enabled en la ruta /vagrant/sites-enabled

--- provision.sh
+++ provision.sh
@@ -9,3 +9,8 @@ rm -r /usr/share/nginx/www
 ln -s /vagrant/www /usr/share/nginx/www
 
 service nginx start
+
+mkdir /vagrant/sites-enabled
+
+cp /etc/nginx/sites-enabled/default /vagrant/sites-enabled
+
